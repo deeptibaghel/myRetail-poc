e1c12bd06e48de36ed433085093e7353d851d01c
deeptibaghel <deeptibaghel@gmail.com>
2019-02-02 20:46:07 +0530

    jwt token revoked check added

--- app/helpers/jwt.js
+++ app/helpers/jwt.js
@@ -1,9 +1,10 @@
 const expressJwt = require('express-jwt');
 const config = require('../../config/jwt.json');
+const userModel = require('../models/user');
 
 module.exports = () => {
   const { secret } = config;
-  return expressJwt({ secret }).unless({
+  return expressJwt({ secret, isRevoked }).unless({
     path: [
       // public routes that don't require authentication
       '/users/authenticate',
@@ -11,3 +12,14 @@ module.exports = () => {
     ]
   });
 };
+
+async function isRevoked(req, payload, done) {
+  const user = await userModel.getById(payload.sub);
+
+  // revoke token if user no longer exists
+  if (!user) {
+    return done(null, true);
+  }
+
+  done();
+}
--- app/models/user.js
+++ app/models/user.js
@@ -2,18 +2,19 @@ const config = require('../../config/jwt.json');
 const jwt = require('jsonwebtoken');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [{ id: 2, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
 
 module.exports = {
   authenticate,
-  getAll
+  getAll,
+  getById
 };
 
 async function authenticate({ username, password }) {
   const user = users.find(u => u.username === username && u.password === password);
   if (user) {
     const token = jwt.sign({ sub: user.id }, config.secret);
-    const { ...userWithoutPassword } = user;
+    const { password, ...userWithoutPassword } = user;
     return {
       ...userWithoutPassword,
       token
@@ -23,7 +24,13 @@ async function authenticate({ username, password }) {
 
 async function getAll() {
   return users.map(u => {
-    const { ...userWithoutPassword } = u;
+    const { password, ...userWithoutPassword } = u;
     return userWithoutPassword;
   });
+}
+
+async function getById(id) {
+  const user = users.find(u => u.id === id);
+  if (user)
+    return true;
 }
